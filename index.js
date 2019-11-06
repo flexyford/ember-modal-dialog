6c8a26c49d3b6a2f679c41dd3888d659abdcc558
Alex Ford <flexyford@users.noreply.github.com>
2019-11-06 16:43:18 -0700

    [DEBUG] Log config path

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ module.exports = {
   name: require('./package').name,
   config: function (env, baseConfig) {
     var configPath = path.join(this.root, 'config', 'environment.js');
+    console.log('configPath = ', configPath);
 
     if (fs.existsSync(configPath)) {
       var configGenerator = require(configPath);
 
+      console.log('found configPath = ', configPath);
       return configGenerator(env, baseConfig, this);
     }
   }
