8bf7d11d5dbf9eee3da300797904dfd863a1ed73
Alex Ford <flexyford@users.noreply.github.com>
2019-11-06 16:59:10 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ module.exports = {
   name: require('./package').name,
   config: function (env, baseConfig) {
     var configPath = path.join(this.root, 'config', 'environment.js');
-    console.log('configPath = ', configPath);
-
+    
     if (fs.existsSync(configPath)) {
       var configGenerator = require(configPath);
-
-      console.log('found configPath = ', configPath);
-      return configGenerator(env, baseConfig, this);
+      const result = configGenerator(env, baseConfig, this);
+      
+      console.log(result);
+      return result;
     }
   }
 
