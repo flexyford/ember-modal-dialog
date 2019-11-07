5a80ddf9fc58e23fdcffc80ef560e88933e77b80
Alex Ford <flexyford@users.noreply.github.com>
2019-11-06 21:19:30 -0700

    fancy logs

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ var fs = require('fs');
 var path = require('path');
 
 module.exports = {
-  name: require('./package').name,
+  name: 'ember-modal-dialog',
   config: function (env, baseConfig) {
+    console.log('[ember-modal-dialog]');
+    
     var configPath = path.join(this.root, 'config', 'environment.js');
-    console.log('built path: ', configPath)
+    console.log('[ember-modal-dialog] configPath = ', configPath);
     
     if (fs.existsSync(configPath)) {
-      console.log('exists: ', configPath)
+      console.log('[ember-modal-dialog] configPath exists');
       var configGenerator = require(configPath);
-      console.log('AND we got a GENERATOR yall!')
+      console.log('[ember-modal-dialog] configPath generated');
       try { 
         const result = configGenerator(env, baseConfig, this);      
-        console.log('ember-modal-dialog attrs: ', result['ember-modal-dialog']);
+        console.log('[ember-modal-dialog] result = ', result['ember-modal-dialog']);
         return result;
       } catch(e) {
         console.log("error = ", e)
