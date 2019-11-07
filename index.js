467a15e638d4690e9f122cb7eccabd48e40be279
Alex Ford <flexyford@users.noreply.github.com>
2019-11-06 22:46:16 -0700

    hardcode version

--- index.js
+++ index.js
@@ -6,29 +6,9 @@ var path = require('path');
 
 module.exports = {
   name: 'ember-modal-dialog',
-  init() {
-    console.log('[ember-modal-dialog] init');
-    this._super.init && this._super.init.apply(this, arguments);
-  },
   config: function (env, baseConfig) {
     console.log('[ember-modal-dialog]');
-    
-    var configPath = path.join(this.root, 'config', 'environment.js');
-    console.log('[ember-modal-dialog] configPath = ', configPath);
-    
-    if (fs.existsSync(configPath)) {
-      console.log('[ember-modal-dialog] configPath exists');
-      var configGenerator = require(configPath);
-      console.log('[ember-modal-dialog] configPath generated');
-      try { 
-        const result = configGenerator(env, baseConfig, this);      
-        console.log('[ember-modal-dialog] result = ', result['ember-modal-dialog']);
-        return result;
-      } catch(e) {
-        console.log("error = ", e)
-        throw e;      
-      }
-    }
+    return { hasEmberTether: "1.0.0" }
   }
 
 };
